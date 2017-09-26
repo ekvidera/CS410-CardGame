3e8f6391759b0906b1a292cb64c4abccd9d9abe8
or9215qv <or9215qv@or9215qv71.workstations.winona.edu>
2017-09-26 13:06:56 -0500

    added GameServer class and UserInterface class; Added getters and setters to the player class;

--- src/util/Player.java
+++ src/util/Player.java
@@ -15,5 +15,35 @@ public class Player {
 		int playerNum;
 		String playerName;
 	}
+	public ArrayList<Card> getHand() {
+		return hand;
+	}
+	public void setHand(ArrayList<Card> hand) {
+		this.hand = hand;
+	}
+	public ArrayList<Card> getCardsWon() {
+		return cardsWon;
+	}
+	public void setCardsWon(ArrayList<Card> cardsWon) {
+		this.cardsWon.addAll(cardsWon);
+	}
+	public int getRoundsWon() {
+		return roundsWon;
+	}
+	public void setRoundsWon(int roundsWon) {
+		this.roundsWon = roundsWon;
+	}
+	public int getPlayerNum() {
+		return playerNum;
+	}
+	public void setPlayerNum(int playerNum) {
+		this.playerNum = playerNum;
+	}
+	public String getPlayerName() {
+		return playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
 	
 }
