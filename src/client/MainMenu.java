1c9f3a896e8370067363bd2128fd6e1dd5726b04
mizuhotakayama <mizuhotakayama@192.168.0.17>
2017-10-07 17:21:44 -0500

    Change images and menu

--- src/client/MainMenu.java
+++ src/client/MainMenu.java
@@ -26,14 +26,14 @@ import java.awt.Color;
 import java.awt.Container;
 
 
-public class UserInterface extends JFrame{
+public class MainMenu extends JFrame{
 
 public static void main(String[] args){
-UserInterface frame = new UserInterface();
+MainMenu frame = new MainMenu();
 
 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 frame.setBounds(10,10,650,650);
-frame.setTitle("Sample!!!");
+frame.setTitle("MainMenu");
 frame.setVisible(true);
 frame.setSize(new Dimension(640, 420));
 frame.setResizable(false);
@@ -52,21 +52,21 @@ h.add(label1);
 }
 
 
-UserInterface(){
+MainMenu(){
 	
-    ImageIcon icon1 = new ImageIcon("resources/NewGame.png");
+    ImageIcon icon1 = new ImageIcon("resources/mainmenu/NewGame.png");
     JButton button1 = new JButton(icon1);
     button1.setBounds(210, 130, 220, 70);
     
-    ImageIcon icon3 = new ImageIcon("resources/Rules.png");
+    ImageIcon icon3 = new ImageIcon("resources/mainmenu/Rules.png");
     JButton button3 = new JButton(icon3);
     button3.setBounds(210, 210, 220, 70);
 
-    ImageIcon icon4 = new ImageIcon("resources/Credit.png");
+    ImageIcon icon4 = new ImageIcon("resources/mainmenu/Credit.png");
     JButton button4 = new JButton(icon4);
     button4.setBounds(210, 290, 100, 70);
     
-    ImageIcon icon2 = new ImageIcon("resources/Quit.png");
+    ImageIcon icon2 = new ImageIcon("resources/mainmenu/Quit.png");
     JButton button2 = new JButton(icon2);
     button2.setBounds(330, 290, 100, 70);
 	
@@ -92,7 +92,7 @@ class MenuPanel extends JPanel {
 Image backgroundImage;
 
 MenuPanel() {
-backgroundImage = Toolkit.getDefaultToolkit().createImage("resources/Background_withTitle.jpg"); 
+backgroundImage = Toolkit.getDefaultToolkit().createImage("resources/mainmenu/Background_withTitle.jpg"); 
 setOpaque(false); 
 }
 
