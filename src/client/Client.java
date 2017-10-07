62b371d0eabda8a509c8ecf1f69613cb4340f0ae
addison <addison@addison-HP-EliteBook-x360-1030-G2>
2017-10-07 17:08:34 -0500

    Added images and stuff

--- src/client/Client.java
+++ src/client/Client.java
@@ -3,6 +3,14 @@ package client;
 import java.net.InetAddress;
 
 public class Client {
+	
 	InetAddress serverAddress;
 	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UserInterface gui = new UserInterface();
+		gui.display();
+		System.out.println("GUI made");
+	}
+
 }
--- src/client/UserInterface.java
+++ src/client/UserInterface.java
@@ -1,5 +1,130 @@
 package client;
 
-public class UserInterface {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+
+import javax.swing.border.LineBorder;
+import javax.swing.border.EtchedBorder;
+import java.awt.Color;
+import java.awt.Container;
+import client.resource.*;
+
+
+public class UserInterface extends JFrame{
+
+public static void main(String[] args){
+UserInterface frame = new UserInterface();
+
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setBounds(10,10,650,650);
+frame.setTitle("Sample!!!");
+frame.setVisible(true);
+frame.setSize(new Dimension(640, 420));
+frame.setResizable(false);
+JPanel h = new JPanel();
+h.setOpaque(false);
+/*
+ImageIcon icon1 = new ImageIcon("Mati.jpg");
+
+JLabel label1 = new JLabel(icon1);
+
+JLabel label2 = new JLabel();
+
+h.add(label1);
+*/
+
+}
+
+
+UserInterface(){
+	
+    ImageIcon icon1 = new ImageIcon("resources/NewGame.png");
+    JButton button1 = new JButton(icon1);
+    button1.setBounds(210, 130, 220, 70);
+    
+    ImageIcon icon3 = new ImageIcon("resources/Rules.png");
+    JButton button3 = new JButton(icon3);
+    button3.setBounds(210, 210, 220, 70);
+
+    ImageIcon icon4 = new ImageIcon("resources/Credit.png");
+    JButton button4 = new JButton(icon4);
+    button4.setBounds(210, 290, 100, 70);
+    
+    ImageIcon icon2 = new ImageIcon("resources/Quit.png");
+    JButton button2 = new JButton(icon2);
+    button2.setBounds(330, 290, 100, 70);
+	
+
+
+JPanel p = new MenuPanel();
+p.setLayout(null);
+
+p.add(button1);
+//p.add(Box.createRigidArea(new Dimension(5,8)));
+p.add(button2);
+//p.add(Box.createRigidArea(new Dimension(5,8)));
+p.add(button3);
+//p.add(Box.createRigidArea(new Dimension(5,8)));
+p.add(button4);
+
+getContentPane().add(p, BorderLayout.CENTER);
+}
+}
+
+
+class MenuPanel extends JPanel {
+Image backgroundImage;
+
+MenuPanel() {
+backgroundImage = Toolkit.getDefaultToolkit().createImage("resources/Background_withTitle.jpg"); 
+setOpaque(false); 
+}
+
+@Override 
+public void paint(Graphics g) {
+g.drawImage(backgroundImage, 0,0, this);
+super.paint(g); 
+this.setSize(new Dimension(640,400));
+}
+
+class NewGame implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
+class Rules implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
+
+class Quit implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
+
+class NewGam implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
 
 }
