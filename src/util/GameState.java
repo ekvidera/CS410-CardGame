385973f6146d683b1b67f65c67fd83e92b3dc97c
Kvidera <ed4481hu@winona.edu>
2017-10-06 19:54:31 -0500

    Modified GameState.Status enum

--- src/util/GameState.java
+++ src/util/GameState.java
@@ -6,6 +6,24 @@ public class GameState implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	public enum Status {
+		STATUS_YOUR_TURN,
+		STATUS_OTHER_PLAYERS_TURN,
+		STATUS_GAME_WON,
+		STATUS_GAME_LOST,
+		STATUS_GAME_TIED,
+		STATUS_GAME_DISCONNECTED,
+	}
+	
+	private Player thisPlayer;
+	private BasicPlayer leftPlayer;
+	private BasicPlayer rightPlayer;
+
+	//cards on table in order:
+	//leftPlayer's card, thisPlayer's card, rightPlayer's card
+	private Card[] cardsOnTable;
+	private Status status;
+	
 	public GameState(Player thisPlayer, BasicPlayer leftPlayer,
 			BasicPlayer rightPlayer, Card[] cardsOnTable, Status status) {
 		
@@ -16,21 +34,25 @@ public class GameState implements Serializable {
 		this.status = status;
 	}
 	
-	Status status;
+	
+	public Player getThisPlayer() {
+		return thisPlayer;
+	}
 
-	Player thisPlayer;
-	BasicPlayer leftPlayer;
-	BasicPlayer rightPlayer;
-	
-	//cards on table in order:
-	//leftPlayer's card, thisPlayer's card, rightPlayer's card
-	Card[] cardsOnTable;
-	
-	public enum Status {
-		STATUS_OK,
-		STATUS_PLAYER_DISCONNECTED,
-		STATUS_WAITING_FOR_PLAYERS,
-		STATUS_GAME_ENDED,
+	public BasicPlayer getLeftPlayer() {
+		return leftPlayer;
+	}
+
+	public BasicPlayer getRightPlayer() {
+		return rightPlayer;
+	}
+
+	public Card[] getCardsOnTable() {
+		return cardsOnTable;
+	}
+
+	public Status getStatus() {
+		return status;
 	}
 	
 }
