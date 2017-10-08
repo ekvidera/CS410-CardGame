81637b35649e065729ad472ec3f015decb367cb8
mizuhotakayama <mizuhotakayama@192.168.0.17>
2017-10-08 12:48:58 -0500

    Update GameUI

--- src/client/GameUI.java
+++ src/client/GameUI.java
@@ -12,6 +12,7 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Label;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.BorderLayout;
@@ -23,9 +24,12 @@ public class GameUI extends JFrame{
 	
 	private static final Component Component = null;
 	String text1;
+    String player1;
+	String player2;
+	String player3;
 	
-	MainMenu ClientName
-	ClientName = new MainMenu();
+	//MainMenu ClientName
+	//ClientName = new MainMenu();
 	
 
 public static void main(String[] args){
@@ -34,7 +38,7 @@ frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 frame.setBounds(10,10,650,650);
 frame.setTitle("Jellyfish Card Game");
 frame.setVisible(true);
-frame.setSize(new Dimension(640, 420));
+frame.setSize(new Dimension(1024, 640));
 frame.setResizable(false);
 JPanel h = new JPanel();
 h.setOpaque(false);
@@ -51,19 +55,26 @@ EventQueue.invokeLater(new Runnable(){
 }
 
 void renderGameState(GameState g) {
-	g.getLeftPlayer().getName();
-	g.getThisPlayer().getName();
-	g.getRightPlaer().
+	
+	
+	player1=g.getLeftPlayer().getName();
+	JLabel label1 = new JLabel ("player1");
+	add(label1);
+	label1.setBounds(10,320,10,10);
+	
+	player2=g.getThisPlayer().getName();
+	JLabel label2 = new JLabel ("player2");
+	add(label2);
+	label1.setBounds(512,630,10,10);
+	
+	player3=g.getRightPlayer().getName();
+	JLabel label3 = new JLabel ("player3");
+	label1.setBounds(1014,320,10,10);
+	add(label3);
 }
+
 GameUI(){
     JPanel g = new JPanel();
-    
-    
-   
-  
-	
-	
-   
     
     //text1 = ButtonListener.clientName;
     JLabel label = new JLabel();
@@ -85,14 +96,14 @@ getContentPane().add(p, BorderLayout.CENTER);
 class GamePanel extends JPanel {
 Image backgroundImage;
 GamePanel() {
-backgroundImage = Toolkit.getDefaultToolkit().createImage("image/GameBackground.png"); 
+backgroundImage = Toolkit.getDefaultToolkit().createImage("resources/GameScreen/GameBackground.png"); 
 setOpaque(false); 
 }
 @Override 
 public void paint(Graphics g) {
 g.drawImage(backgroundImage, 0,0, this);
 super.paint(g); 
-this.setSize(new Dimension(640,400));
+this.setSize(new Dimension(1024,640));
 }
 	  
 }
